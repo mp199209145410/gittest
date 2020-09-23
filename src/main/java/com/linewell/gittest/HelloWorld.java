32ae21222b4ffd105da07e81082f09bd3c6f8501
mp199209145410 <71701663+mp199209145410@users.noreply.github.com>
2020-09-23 22:40:14 +0800

    123
    123

--- src/main/java/com/linewell/gittest/HelloWorld.java
+++ src/main/java/com/linewell/gittest/HelloWorld.java
@@ -2,6 +2,6 @@ package com.linewell.gittest;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-	   System.out.println("right123456");
+	   System.out.println("right123");
 	}
 }
