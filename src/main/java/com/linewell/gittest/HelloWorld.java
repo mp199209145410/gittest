504df15b5c504092e5a99589591e7d4499e54b5a
mp199209145410 <71701663+mp199209145410@users.noreply.github.com>
2020-09-23 22:05:06 +0800

    Update HelloWorld.java
    111

--- src/main/java/com/linewell/gittest/HelloWorld.java
+++ src/main/java/com/linewell/gittest/HelloWorld.java
@@ -2,6 +2,7 @@ package com.linewell.gittest;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+	   System.out.println("Hello World");
+           System.out.println("Hello World123");
 	}
 }
