cb24c0f67fb7c7a73eb58b3dcc2efc01e716fd98
MP <15527671012@163.com>
2020-09-23 22:39:31 +0800

    111

--- src/main/java/com/linewell/gittest/HelloWorld.java
+++ src/main/java/com/linewell/gittest/HelloWorld.java
@@ -2,6 +2,6 @@ package com.linewell.gittest;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-	   System.out.println("right123");
+	   System.out.println("right123456");
 	}
 }
